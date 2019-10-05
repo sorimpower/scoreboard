a7ab5aaf1472b3befcb4ccbaf3dd4c0a272acfe8
sorimpower <dydtjr987987@naver.com>
2019-10-05 13:12:32 +0900

    header에 객체 해체할당 문법 적용

--- src/header.js
+++ src/header.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {Stats} from './stats';
 import {Stopwatch} from "./Stopwatch";
 
-export const Header = (props) => {
+export const Header = (players, title) => {
+	// const {players, title} = props; //props를 객체로 동일한 key 값에 할당, 더간단하게는 위에 쓰면됨
 	return (
 		<header className="header">
-			<Stats players={props.players}/>
-			<h1 className="h1">{props.title}</h1>
+			<Stats players={players}/>
+			<h1 className="h1">{title}</h1>
 			<Stopwatch/>
 		</header>
 	);
