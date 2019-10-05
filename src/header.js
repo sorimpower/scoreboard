ba4d733ac715a875452f0ff7e37ba5fba7a33dea
sorimpower <dydtjr987987@naver.com>
2019-10-05 11:49:56 +0900

    timer 로직 구현완료

--- src/header.js
+++ src/header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Stats} from './stats';
+import {Stopwatch} from "./Stopwatch";
 
 export const Header = (props) => {
-	console.log(props);
 	return (
 		<header className="header">
 			<Stats players={props.players}/>
 			<h1 className="h1">{props.title}</h1>
+			<Stopwatch/>
 		</header>
 	);
 };
